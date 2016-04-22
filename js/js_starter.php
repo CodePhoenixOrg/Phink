38a96ea46ffb58b8212f3885831f4befb294059e
david <dblanchard1@bbox.fr>
2016-04-22 08:15:52 +0200

    Improve JS load
    Improve JS load

--- js/js_starter.php
+++ js/js_starter.php
@@ -6,8 +6,32 @@
  * and open the template in the editor.
  */
 
-$dir = dirname(__FILE__) . DIRECTORY_SEPARATOR;
+
 $destdir = DOCUMENT_ROOT . DIRECTORY_SEPARATOR . 'js' . DIRECTORY_SEPARATOR;
+
+$srcdir =  dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..'  . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'javascript' . DIRECTORY_SEPARATOR . 'thirdparty' . DIRECTORY_SEPARATOR . 'js' . DIRECTORY_SEPARATOR;
+
+$js_filename = $destdir . '_3rdparty.js';
+
+$filenames = [
+        'widgets.js'
+    ,   'jquery.js'
+    ,   'jquery-ui.js'
+    //,   'multiaccordion.jquery.js'
+    ,   'bootstrap.js'
+    //,   'holder.js'
+    //,   'prettify.js'
+    ,   'php.default.min.js'
+//    ,   'drag-and-drop.js'
+];
+
+foreach ($filenames as $filename) {
+    $js_content = file_get_contents($srcdir . $filename, FILE_USE_INCLUDE_PATH);
+    file_put_contents($js_filename, $js_content);
+
+}
+
+$dir = dirname(__FILE__) . DIRECTORY_SEPARATOR;
 
 $js_filename = $destdir . 'code_phoenix.js';
 
@@ -32,26 +56,5 @@ $js_content = '';
 foreach ($filenames as $filename) {
     $js_content .= file_get_contents($dir . $filename);
 }
- 
+
 file_put_contents($js_filename, $js_content);
-
-$srcdir =  dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..'  . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'javascript' . DIRECTORY_SEPARATOR . 'thirdparty' . DIRECTORY_SEPARATOR . 'js' . DIRECTORY_SEPARATOR;
-
-$filenames = [
-        'widgets.js'
-    ,   'jquery.js'
-    ,   'jquery-ui.js'
-    //,   'multiaccordion.jquery.js'
-    ,   'bootstrap.js'
-    //,   'holder.js'
-    //,   'prettify.js'
-    ,   'php.default.min.js'
-//    ,   'drag-and-drop.js'
-];
-
-foreach ($filenames as $filename) {
-    $js_content = file_get_contents($srcdir . $filename, FILE_USE_INCLUDE_PATH);
-    file_put_contents($destdir . $filename, $js_content);
-
-}
-
