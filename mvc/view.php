fc56bf2e6bc506abb9ca478e14a0dc7d3b17fce9
david <dblanchard1@bbox.fr>
2016-09-11 20:34:59 +0200

    Clean View code
    Signed-off-by: david <dblanchard1@bbox.fr>

--- mvc/view.php
+++ mvc/view.php
@@ -6,19 +6,6 @@
  */
 namespace Phink\MVC;
 
-//require_once 'phink/core/object.php';
-//require_once 'phink/core/response.php';
-//require_once 'custom_view.php';
-//require_once 'controller.php';
-//require_once 'phink/utils/file_utils.php';
-
-use Phink\Web\TWebApplication;
-use Phink\Web\TRequest;
-use Phink\Web\TResponse;
-use Phink\MVC\TCustomView;
-use Phink\Utils\TFileUtils;
-use Phink\Tuth\TAuthentication;
-
 /**
  * Description of view
  *
@@ -28,7 +15,7 @@ class TView extends TCustomView
 {
     //put your code here
     
-    public function __construct(TWebApplication $parent)
+    public function __construct(\Phink\Web\TWebApplication $parent)
     {
 
         parent::__construct($parent);
