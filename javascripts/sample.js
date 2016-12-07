48c524e66c29341f49edf197fc34e55969bc9d44
dpjb71 <dpjb78@gmail.com>
2016-12-07 15:56:08 +0100

    Fix sample

--- javascripts/sample.js
+++ javascripts/sample.js
@@ -1,7 +1,7 @@
 var sample = null;
 Phink.DOM.ready(function () {
 
-    sample = TWebApplication.create('sample.com');
+    sample = TWebApplication.create('code-phoenix.org', true);
     sample.main = sample.createView('main');
 
     var sampleMain = sample.createController(sample.main, 'sample.main')
@@ -9,7 +9,7 @@ Phink.DOM.ready(function () {
         goHome : function () {
             sampleMain.getSimpleView('master.html', function(data) {
                 $(document.body).html(data.view);
-                sampleMain.getSimpleView('home.html', function(data) {
+                sampleMain.getSimpleView('sample.html', function(data) {
                     $('#homeContent').html(data.view);
                 });
             });        
