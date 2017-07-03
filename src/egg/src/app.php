fe22c5c3d471ec46f2b429f40c63bfb36084f25c
David Blanchard <dblanchard1@bbox.fr>
2017-07-03 02:18:10 +0200

    Too many things to say now

---
+++ src/egg/src/app.php
@@ -0,0 +1,76 @@
+<?php
+
+/* 
+ * Copyright (C) 2017 dpjb
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+include __DIR__ . '/../../phink_library.php';
+include 'lib.php';
+
+class Egg extends Phink\Core\TApplication {
+
+    /**
+     * Defines the full directory tree of a Phink web application
+     */
+
+    /**
+     * Application starter
+     * 
+     * @param array $argv List of argunments of the command line
+     * @param int $argc Count the number of these arguments
+     */
+    public static function main($args_v, $args_c = 0) {
+        
+        (new Egg($args_v, $args_c));
+        
+    }
+
+    /**
+     * Takes arguments of the command line in parameters.
+     * The start make this job fine.
+     * 
+     * @param array $argv List of argunments of the command line
+     * @param int $argc Count the number of these arguments
+     */
+    public function __construct($args_v, $args_c = 0)
+    {
+        parent::__construct($args_v, $args_c, __DIR__);
+        
+        try {
+            $egg = new EggLib();
+            
+//            if(!class_exists('\Phink\TAutoloader')) {
+//                $this->_requirePhink();
+//            }
+//            
+            
+            if ($this->getArgument('create')) {
+                $egg->create();
+            }
+
+            if ($this->getArgument('delete')) {
+                $egg->delete();
+            }
+
+        } catch (Exception $ex) {
+            echo $ex->getMessage() . PHP_EOL;
+        }
+    }
+
+   
+}
+ 
+Egg::main($argv, $argc);
---
+++ src/phink/utils/zip.php
@@ -0,0 +1,119 @@
+<?php
+
+/*
+ * Copyright (C) 2017 dpjb
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Phink\Utils;
+
+/**
+ * Description of zip
+ *
+ * @author dpjb
+ */
+class Zip {
+    //put your code here
+    function deflat($src_file, $dest_dir=false, $create_zip_name_dir=true, $overwrite=true) 
+    {
+        if ($zip = zip_open($src_file)) 
+        {
+            if ($zip) 
+            {
+                $splitter = ($create_zip_name_dir === true) ? "." : "/";
+                if ($dest_dir === false) $dest_dir = substr($src_file, 0, strrpos($src_file, $splitter))."/";
+
+                // Create the directories to the destination dir if they don't already exist
+                if(!file_exists($dest_dir)) {
+                    mkdir($dest_dir, 0777, true);
+                }
+
+                // For every file in the zip-packet
+                while ($zip_entry = zip_read($zip)) 
+                {
+                    // Now we're going to create the directories in the destination directories
+
+                    // If the file is not in the root dir
+                    $pos_last_slash = strrpos(zip_entry_name($zip_entry), "/");
+                    if ($pos_last_slash !== false)
+                    {
+                        // Create the directory where the zip-entry should be saved (with a "/" at the end)
+                        $path = $dest_dir.substr(zip_entry_name($zip_entry), 0, $pos_last_slash+1);
+                        if(!file_exists($path)) {
+                            mkdir($path, 0777, true);
+                        }
+                    }
+
+                    // Open the entry
+                    if (zip_entry_open($zip,$zip_entry,"r")) 
+                    {
+
+                        // The name of the file to save on the disk
+                        $file_name = $dest_dir.zip_entry_name($zip_entry);
+
+                        // Check if the files should be overwritten or not
+                        if ($overwrite === true || $overwrite === false && !is_file($file_name))
+                        {
+                            // Get the content of the zip entry
+                            $fstream = zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
+
+                            file_put_contents($file_name, $fstream );
+                            // Set the rights
+                            chmod($file_name, 0777);
+                        }
+
+                        // Close the entry
+                        zip_entry_close($zip_entry);
+                    }
+                }
+                // Close the zip-file
+                zip_close($zip);
+            }
+        } 
+        else
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * This function creates recursive directories if it doesn't already exist
+     *
+     * @param String  The path that should be created
+     *  
+     * @return  void
+     */
+//    private function create_dirs($path)
+//    {
+//      if (!is_dir($path))
+//      {
+//        $directory_path = "";
+//        $directories = explode("/",$path);
+//        array_pop($directories);
+//
+//        foreach($directories as $directory)
+//        {
+//          $directory_path .= $directory."/";
+//          if (!is_dir($directory_path))
+//          {
+//            mkdir($directory_path);
+//            chmod($directory_path, 0777);
+//          }
+//        }
+//      }
+//    }    
+}
