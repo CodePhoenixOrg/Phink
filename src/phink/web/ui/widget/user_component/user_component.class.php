8af155547c49c5457ec946e7ae5545835c64db4a
david-blanchard <dblanchard1@bbox.fr>
2019-10-25 19:48:17 +0200

    Simplifiy the load of a component

--- src/phink/web/ui/widget/user_component/user_component.class.php
+++ src/phink/web/ui/widget/user_component/user_component.class.php
@@ -15,12 +15,11 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
 namespace Phink\Web\UI\Widget\UserComponent;
 
-use Phink\TAutoloader;
 use Phink\Core\IObject;
-use Phink\Registry\TRegistry;
+use Phink\TAutoloader;
 use Phink\Web\UI\TCustomControl;
 
 class TUserComponent extends TCustomControl
@@ -37,50 +36,48 @@ class TUserComponent extends TCustomControl
 
     }
 
-    public function setComponentType(string $value) : void
+    public function setComponentType(string $value): void
     {
         $this->componentType = $value;
     }
-    
-    public function setComponentId(string $value) : void
+
+    public function setComponentId(string $value): void
     {
         $this->componentId = $value;
     }
 
-    public function render() : void 
+    public function render(): void
     {
         $mvcFileNames = $this->getMvcFileNamesByTypeName($this->componentType);
 
         // foreach ($mvcFileNames as $key => $name) {
-        //     echo $key . '::' . $name . '<br />'; 
+        //     echo $key . '::' . $name . '<br />';
         // }
 
         $cachedFileName = $mvcFileNames['cacheFileName'];
         $controllerFileName = $mvcFileNames['controllerFileName'];
 
         $code = '';
+        $fqClassName = '';
 
-        if(file_exists($cachedFileName)) {
+        if (file_exists($cachedFileName)) {
             list($namespace, $className, $code) = TAutoloader::getClassDefinition($cachedFileName);
-            
-            include $cachedFileName;
 
+            $controllerFileName = $cachedFileName;
             $fqClassName = $namespace . '\\' . $className;
-
-            $class = new $fqClassName($this->getParent());
-            $class->render();
         }
 
-        if(!file_exists($cachedFileName)) {
-            
-            list($controllerFileName, $className, $code) = TAutoloader::includeClass($controllerFileName, RETURN_CODE | INCLUDE_FILE);
-
-            include $controllerFileName;
-    
-            $class = new $className($this->getParent());
-            $class->render();
+        if (!file_exists($cachedFileName)) {
+            list($controllerFileName, $fqClassName, $code) = TAutoloader::includeClass($controllerFileName, RETURN_CODE | INCLUDE_FILE);
         }
-        
+        include $controllerFileName;
+
+        self::getLogger()->debug('USER COMPONENT CONTROLLER FILE NAME::' . $controllerFileName);
+        self::getLogger()->debug('USER COMPONENT CACHED FILE EXISTS::' . (file_exists($cachedFileName) ? 'TRUE' : 'FALSE'));
+
+        $class = new $fqClassName($this->getParent());
+        $class->render();
+
     }
 
 }
