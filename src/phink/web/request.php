1f06f066fc60c4b570551067da8058f481e0cef9
david-blanchard <dblanchard1@bbox.fr>
2019-11-01 23:36:27 +0100

    GetArgument can now manage arrays of from inputs

--- src/phink/web/request.php
+++ src/phink/web/request.php
@@ -306,15 +306,22 @@ class TRequest extends \Phink\Core\TObject
         return $result;
     }
 
-    public static function getArgument($arg, $default = ''): string
+    public static function getArgument($arg, $default = '')
     {
         $result = '';
 
         // mysql_escape_string
+        if (isset($_POST[$arg])) {
+            $result = filter_input(INPUT_POST, $arg, FILTER_SANITIZE_STRING);
+            if(is_array($_POST[$arg])) {
+                $result = filter_input(INPUT_POST, $arg, FILTER_SANITIZE_STRING, FILTER_REQUIRE_ARRAY);
+            }
+            return $result;
+        }
+
         if (isset($_GET[$arg])) {
             $result = filter_input(INPUT_GET, $arg, FILTER_SANITIZE_STRING);
-        } elseif (isset($_POST[$arg])) {
-            $result = filter_input(INPUT_POST, $arg, FILTER_SANITIZE_STRING);
+            return $result;
         }
 
         if ($result === '') {
