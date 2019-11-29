20c4dd8e73cda20ff2fe653388c5d8a12573b927
david-blanchard <dblanchard1@bbox.fr>
2019-11-29 19:24:52 +0100

    Allow empty result but still need a fix

--- src/phink/web/ui/plugin/table.php
+++ src/phink/web/ui/plugin/table.php
@@ -15,14 +15,14 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
- 
- 
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 namespace Phink\Web\UI\Plugin;
+
 /**
  * Description of newPHPClass
  *
@@ -33,68 +33,64 @@ class TTable extends TCustomPlugin
     //put your code here
     public function render()
     {
-        
+
         $elements = $this->elements;
-        $noTHead = false; 
+        $noTHead = false;
 
         $result = "\n";
 
         $head = $this->data['names'];
 
         $result .= str_replace('%s', 'id="' . $this->getId() . $elements[0]->getType() . '" class="table table-striped table-hover table-condensed"', $elements[0]->getOpening()) . "\n";
-        /** 
+        /**
          * header
          */
-        $i = 0;
         $thead = $elements[1]->getOpening() . "\n";
-        $typeId0 = 'id="' . $this->getId() .  $elements[3]->getType() . ($i) . '"';
+        $typeId0 = 'id="' . $this->getId() . $elements[3]->getType() . (0) . '"';
         $thead .= str_replace('%s', $typeId0, $elements[3]->getOpening()) . "\n";
-        for($j = 0; $j < $this->columns; $j++) {
-            $k = $i * $this->columns + $j;
-            $colName = $this->templates[$j]['name'];
+        for ($j = 0; $j < $this->columns; $j++) {
+            $colName = $head[$j];
+            if($this->templates[0]['name'] != '*') {
+                $colName = $this->templates[$j]['name'];
+            }
             $colIndex = array_keys($head, $colName)[0];
-            $typeId1 = 'id="' . $this->getId() .  $elements[4]->getType() . $k . '"';
+            $typeId1 = 'id="' . $this->getId() . $elements[4]->getType() . $j . '"';
             $thead .= str_replace('%s', $typeId1, $elements[4]->getOpening()) . $head[$colIndex] . $elements[4]->getClosing() . "\n";
-
         }
+
         $thead .= $elements[3]->getClosing() . "\n";
         $thead .= $elements[1]->getClosing() . "\n";
 
-        if(!$this->pivot) {
-
-                
-            /** 
+        if (!$this->pivot) {
+            /**
              * values
              */
-              
+
             $tbody = $elements[2]->getOpening() . "\n";
             $body = $this->data['values'];
-            for($i = 0; $i < $this->rows; $i++) {
+            for ($i = 0; $i < $this->rows; $i++) {
 
-                // $row = (isset($body[$i])) ? json_decode($body[$i]) : array_fill(0, $this->columns, '&nbsp;');
                 $row = (isset($body[$i])) ? $body[$i] : array_fill(0, $this->columns, '&nbsp;');
-                $typeId0 = 'id="' . $this->getId() .  $elements[3]->getType() . ($i) . '"';
+                $typeId0 = 'id="' . $this->getId() . $elements[3]->getType() . ($i) . '"';
                 $tbody .= str_replace('%s', $typeId0, $elements[3]->getOpening()) . "\n";
-                
-                // $this->getLogger()->debug('** BEGIN **');
-                // $this->getLogger()->debug($head);
-                // $this->getLogger()->debug($row);
-                // $this->getLogger()->debug('** END **');
-            
-                for($j = 0; $j < $this->columns; $j++) {
-                    $k = $i * $this->columns + $j;
-                    $dataIndex = array_keys($head, $this->templates[$j]['name'])[0];
 
-                    $noTHead = !empty($this->templates[$j]['content']) && $this->templates[$j]['enabled'] == 1;
-                    
-                    // $this->getLogger()->debug('NO THEAD: ' . \boolval($noTHead), __FILE__, __LINE__);
+                for ($j = 0; $j < $this->columns; $j++) {
+                    $k = $i * $this->columns + $j;
+                    $colIndex = 0;
+                    $dataIndex = array_keys($head, $head[$j])[0];
+                    if($this->templates[0]['name'] != '*') {
+                        $dataIndex = array_keys($head, $this->templates[$j]['name'])[0];
+                        $colIndex = $dataIndex;
+                    }
+
+                    $noTHead = isset($this->templates[$j]) && !empty($this->templates[$j]['content']) && $this->templates[$j]['enabled'] == 1;
 
                     $html = $row[$dataIndex];
-                    if($noTHead) {
+                    if ($noTHead) {
                         $html = \Phink\Web\UI\Widget\Plugin\TPlugin::applyTemplate($this->templates, $head, $row, $j);
                     }
-                    if($this->templates[$colIndex]['enabled'] == 1) {
-                        $typeId1 = 'id="' . $this->getId() .  $elements[5]->getType() . $k . '"';
+                    if (isset($this->templates[$colIndex]) && $this->templates[$colIndex]['enabled'] == 1) {
+                        $typeId1 = 'id="' . $this->getId() . $elements[5]->getType() . $k . '"';
                         $tbody .= str_replace('%s', $typeId1, $elements[5]->getOpening()) . $html . $elements[5]->getClosing() . "\n";
                     }
                 }
@@ -103,28 +99,30 @@ class TTable extends TCustomPlugin
             $tbody .= $elements[2]->getClosing() . "\n";
 
             $result .= (($noTHead) ? '' : $thead) . $tbody;
-        } else {
+        }
+
+        if ($this->pivot) {
             /**
              * values
              */
-             
+
             $tbody = $elements[2]->getOpening() . "\n";
             $body = $this->data['values'];
             $oldValue = array();
-            for($i = 0; $i < $this->rows; $i++) {
+            for ($i = 0; $i < $this->rows; $i++) {
 
                 $row = (isset($body[$i])) ? json_decode($body[$i]) : array_fill(0, $this->columns, '&nbsp;');
-                $typeId0 = 'id="' . $this->getId() .  $elements[3]->getType() . ($i) . '"';
+                $typeId0 = 'id="' . $this->getId() . $elements[3]->getType() . ($i) . '"';
                 $tbody .= str_replace('%s', $typeId0, $elements[3]->getOpening()) . "\n";
-                for($j = 0; $j < $this->columns; $j++) {
+                for ($j = 0; $j < $this->columns; $j++) {
                     $k = $i * $this->columns + $j;
                     $noTHead = $this->templates[$j]['content'] && $this->templates[$j]['enabled'] == 1;
                     $html = \Phink\Web\UI\Widget\Plugin\TPlugin::applyTemplate($this->templates, $row, $j);
                     $typeId1 = 'id="' . $this->getId() . $elements[5]->getType() . $k . '"';
                     $name = $this->templates[$j]['name'];
-                    
-                    if($this->templates[$j]['enabled'] == 1 && $row[$j] != $oldValue[$j]) {
-                        
+
+                    if ($this->templates[$j]['enabled'] == 1 && $row[$j] != $oldValue[$j]) {
+
                         $tbody .= $elements[3]->getOpening();
                         $tbody .= str_replace('%s', $typeId1, $elements[5]->getOpening()) . $html . $elements[5]->getClosing() . "\n";
                         $tbody .= $elements[3]->getClosing();
@@ -138,8 +136,8 @@ class TTable extends TCustomPlugin
             $result .= (($noTHead) ? '' : $thead) . $tbody;
         }
         $result .= $elements[0]->getClosing() . "\n";
-        
+
         return $result;
-       
+
     }
 }
