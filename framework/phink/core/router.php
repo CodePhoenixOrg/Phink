2e40517210ca414b915939c65a7028b68e87d89c
blanchard <david.blanchard1@cewe.fr>
2020-03-29 16:08:41 +0200

    Fix routes

--- framework/phink/core/router.php
+++ framework/phink/core/router.php
@@ -155,15 +155,14 @@ class TRouter extends TObject implements \Phink\Web\IWebObject
         $routesArray['web']['get']["^/console/([a-z-]+)$"] = "@/console/app/views/console.phtml?console=$1";
         $routesArray['web']['get']["^/console/([a-z-]+)/([a-z-]+)$"] = "@/console/app/views/console.phtml?console=$1&arg=$2";
         $routesArray['web']['post']["^/console$"] = "@/console/app/views/console_window.phtml";
-        $routesArray['web']['get']["^/tuto/$"] = "@/tuto/app/views/index.phtml";
-        $routesArray['web']['get']["^/admin$"] = "@/admin/app/views/page.phtml?di=mkmain";
+        $routesArray['web']['get']["^/tuto(/)?$"] = "@/tuto/app/views/index.phtml";
+        $routesArray['web']['get']["^/admin(/)?$"] = "@/admin/app/views/page.phtml?di=mkmain";
         $routesArray['web']['get']["^/admin(\\?([a-zA-Z0-9\._\-=&]+))?$"] = "@/admin/app/views/page.phtml?$2";
         $routesArray['web']['post']["^/admin(\\?([a-zA-Z0-9\._\-=&]+))?$"] = "@/admin/app/views/page.phtml?$2";
         $routesArray['web']['get']["^/page.html(\\?([a-zA-Z0-9\._\-=&]+))?$"] = "@/admin/app/views/page.phtml?$2";
         $routesArray['web']['post']["^/page.html(\\?([a-zA-Z0-9\._\-=&]+))?$"] = "@/admin/app/views/page.phtml?$2";
         $routesArray['web']['get']["^/admin/source(\\?([a-z0-9\._\-=&]+))?$"] = "@/admin/app/views/source.phtml?$2";
-        $routesArray['web']['get']["^/qbe$"] = "@/qbe/app/views/qbe.phtml";
-        $routesArray['web']['get']["^/qbe/$"] = "@/qbe/app/views/qbe.phtml?qbe=help";
+        $routesArray['web']['get']["^/qbe(/)?$"] = "@/qbe/app/views/qbe.phtml";
         $routesArray['web']['get']["^/qbe/([a-z-]+)$"] = "@/qbe/app/views/qbe.phtml?qbe=$1";
         $routesArray['web']['get']["^/qbe/([a-z-]+)/([a-z-]+)$"] = "@/qbe/app/views/qbe.phtml?qbe=$1&arg=$2";
         $routesArray['web']['post']["^/qbe$"] = "@/qbe/app/views/qbe_window.phtml";
