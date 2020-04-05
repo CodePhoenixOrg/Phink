0502ccea9066eec797a8789e11c4efa241e051f2
blanchard <david.blanchard1@cewe.fr>
2020-04-05 19:51:11 +0200

    Don't load unecessary files

--- src/web/bootstrap.php
+++ src/web/bootstrap.php
@@ -1,5 +1,4 @@
 <?php
-// include '../../framework/phink/phink_library.php';
-// include '../../framework/plugins/plugins_library.php';
-// include '../../framework/puzzle/ipuzzle_library.php';
-include '../../vendor/autoload.php';
+include '../../framework/phink/phink_library.php';
+include '../../framework/plugins/plugins_library.php';
+// include '../../vendor/autoload.php';
