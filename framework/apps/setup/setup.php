e1adb371163037081fa8a4aacc33242cab1372fd
blanchard <david.blanchard1@cewe.fr>
2020-04-09 18:32:38 +0200

    Fix embedded PHP server bootstrap

--- framework/apps/setup/setup.php
+++ framework/apps/setup/setup.php
@@ -151,8 +151,8 @@ if(\$is127 || \$isIndex) {
     header('Location: //' . \$hostname . \$port . \$requestUri);
     exit(302);
 }
-define('CONFIG_DIR', '..' . DIRECTORY_SEPARATOR . 'config');
-define('FRAMEWORK', '../../' . trim(file_get_contents(CONFIG_DIR . 'framework')));
+define('CONFIG_DIR', '..' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR);
+define('FRAMEWORK', '..'  . DIRECTORY_SEPARATOR . '..'  . DIRECTORY_SEPARATOR . trim(file_get_contents(CONFIG_DIR . 'framework')));
 include FRAMEWORK . 'phink' . DIRECTORY_SEPARATOR . 'phink_library.php';
 include FRAMEWORK . 'plugins' . DIRECTORY_SEPARATOR . 'plugins_library.php';
         
