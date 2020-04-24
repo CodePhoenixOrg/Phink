3d35da3119318fb3fe3d4fd0b276f12860e052e0
david-blanchard <dpjb@hotmail.fr>
2020-04-24 08:58:15 +0200

    Fix the bootstrap

--- src/web/bootstrap.php
+++ src/web/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 define('CONFIG_DIR', '..' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR);
-define('FRAMEWORK', '..'  . DIRECTORY_SEPARATOR . '..'  . DIRECTORY_SEPARATOR . trim(file_get_contents(CONFIG_DIR . 'framework')));
+define('FRAMEWORK', trim(file_get_contents(CONFIG_DIR . 'framework')));
 // include FRAMEWORK . 'phink' . DIRECTORY_SEPARATOR . 'phink_library.php';
 // include FRAMEWORK . 'plugins' . DIRECTORY_SEPARATOR . 'plugins_library.php';
 include '../../vendor/autoload.php';
